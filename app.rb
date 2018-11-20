8a15351ebe0c80a31a084e511b045f924d0fc743
Sanjay Sai <shachsan@hotmail.com>
2018-11-20 01:04:49 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+
+    params.each do |k, v|
+      instance_variable_set('@' + k, v)
+    end
+    erb :team
+  end
 
 end
